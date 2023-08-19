0893c7531fdbb5ec861665509b334baa99c49283
mustafa <mustafa@tayfargem.com.tr>
2023-08-19 13:33:33 +0300

    readme guncellendi, data okuma taslak eklendi

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# kdk
-Kamu Denetçiliği Kurumu Başvuru Yönetim ve Karar Destek Sistemi
+# kdk_by_kds
+
+## Kamu Denetçiliği Kurumu Başvuru Yönetim ve Karar Destek Sistemi
+
+- [ ] web sunucusundan tüm yıllara ait verilerin alınıp .csv formatında kaydedilmesi.  
+- [ ] Veri analizi  
+- [ ] git reposunun düzenlenmesi  
---
+++ kdk_by_kds/data/get_data.py
@@ -0,0 +1,21 @@
+"""
+KDK Karar Verilerini İnternetten Alma Modülü
+
+https://kararlar.ombudsman.gov.tr/Arama/Index
+
+Kaynaklar:
+    Datacamp - Web Scraping in Python
+
+"""
+import requests
+from scrapy import Selector
+from scrapy.crawler import CrawlerProcess
+
+url = 'https://kararlar.ombudsman.gov.tr/Arama/Index'
+
+html = requests.get(url).content
+# POST start, length
+# draw=3&columns%5B0%5D%5Bdata%5D=sikayeT_NO&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=karaR_TARIH&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=karaR_TURU&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=evraK_KONU&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=sikayeT_EDILEN_KURUM&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=evraK_ID&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=desc&start=20&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&basbas=&basbit=&basnoyil=&basnosayi=&sikayetkonu=&konuidarekelimeleri=&karbas=&karbit=&kararTuru=&evrakkonu=&ortakalankelimeleri=
+
+sel = Selector(text=html)
+
